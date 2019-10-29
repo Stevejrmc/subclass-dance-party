6985b4f7dbb93940b443b4d514e49df3a3e223bf
stevejrmc <stevejrmc@gmail.com>
2019-10-29 15:42:00 -0700

    removed debugger

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,6 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    debugger;
     blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
